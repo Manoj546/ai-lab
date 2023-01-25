9569de53ce7cbe8330e8368c3eaa3889f8530c58
Manoj <84076478+Manoj546@users.noreply.github.com>
2023-01-25 23:36:06 +0530

    final

--- 02_8 Puzzle/8 Puzzle BFS/8_puzzle_bfs.py
+++ 02_8 Puzzle/8 Puzzle BFS/8_puzzle_bfs.py
@@ -1,97 +1,135 @@
-from queue import Queue
+import numpy as np
+import sys
+
+class Node:
+    def __init__(self, state, parent, action):
+        self.state = state
+        self.parent = parent
+        self.action = action
+
+
+class StackFrontier:
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node):
+        self.frontier.append(node)
+
+    def contains_state(self, state):
+        return any((node.state[0] == state[0]).all() for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def remove(self):
+        if self.empty():
+            raise Exception("Empty Frontier")
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
+
+
+class QueueFrontier(StackFrontier):
+    def remove(self):
+        if self.empty():
+            raise Exception("Empty Frontier")
+        else:
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]
+            return node
+
 
 class Puzzle:
-    goal_state=[1,2,3,5,8,6,0,7,4]
-    heuristic=None
-    evaluation_function=None
-    num_of_instances=0
-    def __init__(self,state,parent,action,path_cost):
-        self.parent=parent
-        self.state=state
-        self.action=action
-        if parent:
-            self.path_cost = parent.path_cost + path_cost
-        else:
-            self.path_cost = path_cost
-        Puzzle.num_of_instances+=1
+    def __init__(self, start, startIndex, goal, goalIndex):
+        self.start = [start, startIndex]
+        self.goal = [goal, goalIndex]
+        self.solution = None
 
-    def __str__(self):
-        return str(self.state[0:3])+'\n'+str(self.state[3:6])+'\n'+str(self.state[6:9])
+    def neighbors(self, state):
+        mat, (row, col) = state
+        results = []
 
-    def goal_test(self):
-        if self.state == self.goal_state:
-            return True
-        return False
+        if row > 0:
+            mat1 = np.copy(mat)
+            mat1[row][col] = mat1[row - 1][col]
+            mat1[row - 1][col] = 0
+            results.append(('up', [mat1, (row - 1, col)]))
+        if col > 0:
+            mat1 = np.copy(mat)
+            mat1[row][col] = mat1[row][col - 1]
+            mat1[row][col - 1] = 0
+            results.append(('left', [mat1, (row, col - 1)]))
+        if row < 2:
+            mat1 = np.copy(mat)
+            mat1[row][col] = mat1[row + 1][col]
+            mat1[row + 1][col] = 0
+            results.append(('down', [mat1, (row + 1, col)]))
+        if col < 2:
+            mat1 = np.copy(mat)
+            mat1[row][col] = mat1[row][col + 1]
+            mat1[row][col + 1] = 0
+            results.append(('right', [mat1, (row, col + 1)]))
 
-    @staticmethod
-    def find_legal_actions(i,j):
-        legal_action = ['U', 'D', 'L', 'R']
-        if i == 0:  # up is disable
-            legal_action.remove('U')
-        elif i == 2:  # down is disable
-            legal_action.remove('D')
-        if j == 0:
-            legal_action.remove('L')
-        elif j == 2:
-            legal_action.remove('R')
-        return legal_action
+        return results
 
-    def generate_child(self):
-        children=[]
-        x = self.state.index(0)
-        i = int(x / 3)
-        j = int(x % 3)
-        legal_actions=self.find_legal_actions(i,j)
+    def print(self):
+        solution = self.solution if self.solution is not None else None
+        print("Start State:\n", self.start[0], "\n")
+        print("Goal State:\n", self.goal[0], "\n")
+        print("Solution:\n ")
+        for action, cell in zip(solution[0], solution[1]):
+            print("action: ", action, "\n", cell[0], "\n")
+        print("---Goal Matrix obtained---")
 
-        for action in legal_actions:
-            new_state = self.state.copy()
-            if action == 'U':
-                new_state[x], new_state[x-3] = new_state[x-3], new_state[x]
-            elif action == 'D':
-                new_state[x], new_state[x+3] = new_state[x+3], new_state[x]
-            elif action == 'L':
-                new_state[x], new_state[x-1] = new_state[x-1], new_state[x]
-            elif action == 'R':
-                new_state[x], new_state[x+1] = new_state[x+1], new_state[x]
-            children.append(Puzzle(new_state,self,action,1))
-        return children
+    def does_not_contain_state(self, state):
+        for st in self.explored:
+            if (st[0] == state[0]).all():
+                return False
+        return True
 
-    def find_solution(self):
-        solution = []
-        solution.append(self.action)
-        path = self
-        while path.parent != None:
-            path = path.parent
-            solution.append(path.action)
-        solution = solution[:-1]
-        solution.reverse()
-        return solution
+    def solve(self):
+        self.num_explored = 0
 
-def breadth_first_search(initial_state):
-    start_node = Puzzle(initial_state, None, None, 0)
-    if start_node.goal_test():
-        return start_node.find_solution()
-    q = Queue()
-    q.put(start_node)
-    explored=[]
-    while not(q.empty()):
-        node=q.get()
-        explored.append(node.state)
-        children=node.generate_child()
-        for child in children:
-            if child.state not in explored:
-                node.__str__()
-                if child.goal_test():
-                    return child.find_solution()
-                q.put(child)
-    return
+        start = Node(state=self.start, parent=None, action=None)
+        frontier = QueueFrontier()
+        frontier.add(start)
 
-state=[1, 2, 3,
-        5, 6, 0,
-        7, 8, 4]
+        self.explored = []
 
-Puzzle.num_of_instances=0
-bfs=breadth_first_search(state)
-print('BFS:', bfs)
-print('space:',Puzzle.num_of_instances)
-print()
+        while True:
+            if frontier.empty():
+                raise Exception("No solution")
+
+            node = frontier.remove()
+            self.num_explored += 1
+
+            if (node.state[0] == self.goal[0]).all():
+                actions = []
+                cells = []
+                while node.parent is not None:
+                    actions.append(node.action)
+                    cells.append(node.state)
+                    node = node.parent
+                actions.reverse()
+                cells.reverse()
+                self.solution = (actions, cells)
+                return
+
+            self.explored.append(node.state)
+
+            for action, state in self.neighbors(node.state):
+                if not frontier.contains_state(state) and self.does_not_contain_state(state):
+                    child = Node(state=state, parent=node, action=action)
+                    frontier.add(child)
+
+
+start = np.array([[1, 2, 3], [8, 0, 4], [7, 6, 5]])
+goal = np.array([[2, 8, 1], [0, 4, 3], [7, 6, 5]])
+
+startIndex = (1, 1)
+goalIndex = (1, 0)
+
+p = Puzzle(start, startIndex, goal, goalIndex)
+p.solve()
+p.print()
